a614865da91b170918029c007eb9d6e4a08fa53f
sifhat <apple@apples-MacBook-Air.local>
2025-10-22 18:49:55 +0530

    mobile responsiveness

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,10 +20,10 @@ function App() {
       <Router>
         <div className="flex h-screen bg-gray-50">
           <Sidebar />
-          <div className="flex-1 flex flex-col overflow-hidden ml-64">
-            <header className="bg-white shadow-sm border-b border-gray-200">
+          <div className="flex-1 flex flex-col overflow-hidden ml-0 sm:px-0 lg:ml-63 ">
+            <header className="bg-white shadow-sm border-b border-gray-200 sm:px-0">
               <div className="px-6 py-4">
-                <h2 className="text-2xl font-semibold text-gray-800">OR Management System</h2>
+                <h2 className="text-2xl font-semibold text-gray-800 mt-10 lg:mt-0">OR Management System</h2>
               </div>
             </header>
             <main className="flex-1 overflow-y-auto p-6">
