8c414934b670c2fc013d9c1b003c1f531b7da99d
Atharv Batule <atharvbatule9@.com>
2025-10-24 20:31:49 +0530

    update db.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,17 +1,29 @@
+// import { Client } from 'pg';
+
+// const client = new Client({
+//   host: 'db.glkubgmswyjawlcgxspw.supabase.co',
+//   port: 6543,
+//   user: 'postgres',
+//   password: 'DmbgELe4GarxXkS5',
+//   database: 'postgres',
+//   ssl: { rejectUnauthorized: false }
+// });
+
+// client.connect()
+//   .then(() => {
+//     console.log('Connected to PostgreSQL');
+//   })
+
+//   export {client};
 import { Client } from 'pg';
 
 const client = new Client({
-  host: 'db.glkubgmswyjawlcgxspw.supabase.co',
-  port: 6543,
-  user: 'postgres',
-  password: 'DmbgELe4GarxXkS5',
-  database: 'postgres',
-  ssl: { rejectUnauthorized: false }
+  connectionString: 'postgresql://postgres:DmbgELe4GarxXkS5@db.glkubgmswyjawlcgxspw.supabase.co:5432/postgres',
+  ssl: { rejectUnauthorized: false } // Required for Supabase
 });
 
 client.connect()
-  .then(() => {
-    console.log('Connected to PostgreSQL');
-  })
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error('Connection error', err));
 
-  export {client};
+export { client };
